65e8fc671223d43439db593b78cc3d6b6cf2210d
psikomonkie <189469115+psikomonkie@users.noreply.github.com>
2026-02-23 12:03:27 -0500

    PR: Throw & handle exception if MekHQ tries to remove more armor than possible - we need to fix the root cause of this!!

--- MekHQ/src/mekhq/campaign/parts/Armor.java
+++ MekHQ/src/mekhq/campaign/parts/Armor.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2009 Jay Lawson (jaylawson39 at yahoo.com). All rights reserved.
- * Copyright (C) 2013-2025 The MegaMek Team. All Rights Reserved.
+ * Copyright (C) 2013-2026 The MegaMek Team. All Rights Reserved.
  *
  * This file is part of MekHQ.
  *
@@ -637,10 +637,16 @@ public class Armor extends Part implements IAcquisitionWork {
             amountRemaining = changeAmountAvailableSingle(amountRemaining);
         }
 
-        if (amountRemaining > 0) {
-            LOGGER.warn("Still trying to add armor but that shouldn't have been a problem!");
-        } else if (amount < 0) {
-            LOGGER.warn("Still trying to remove armor but no more armor is in the warehouse!");
+        try {
+            if (amountRemaining > 0) {
+                throw new IncompletePartUpdateException(
+                      "Still trying to add armor but that shouldn't have been a problem!");
+            } else if (amount < 0) {
+                throw new IncompletePartUpdateException(
+                      "Still trying to remove armor but no more armor is in the warehouse!");
+            }
+        } catch (IncompletePartUpdateException e) {
+            LOGGER.error(e.getMessage(), e);
         }
     }
 
