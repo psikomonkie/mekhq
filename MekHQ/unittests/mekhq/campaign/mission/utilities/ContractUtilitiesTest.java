2d03590fc7c1cc4a5e535f473d3bc16684ec4e53
IllianiBird <danielfarbrace@icloud.com>
2026-07-06 21:00:13 -0400

    Adjustment

--- MekHQ/unittests/mekhq/campaign/mission/utilities/ContractUtilitiesTest.java
+++ MekHQ/unittests/mekhq/campaign/mission/utilities/ContractUtilitiesTest.java
@@ -54,7 +54,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class ClanHomeworldsExclusionTest {
+public class ContractUtilitiesTest {
     static Campaign mockCampaign;
 
     ArrayList<CombatTeam> mockCombatTeams;
